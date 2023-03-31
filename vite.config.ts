656af350e473d76c37b5d7d18de62b9d60fa02be
ryinner <vinograd18062002@gmail.com>
2023-04-01 00:08:38 +0500

    intall vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import { defineConfig } from 'vite';
+
+const appDir = path.resolve(__dirname);
+const srcDir = path.join(appDir, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': srcDir
+        },
+    },
 });
