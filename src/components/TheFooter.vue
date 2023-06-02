b57152c93b5d33f3ef8a28441ce2aa0f8296ef15
ryinner <vinograd18062002@gmail.com>
2023-06-02 11:36:19 +0500

    add link to icons

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer class="footer">
+        The best icons here: <a href="https://icons8.com/">https://icons8.com/</a>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+    margin-top: 20px;
+
+    width: 100%;
+
+    font-size: 0.8rem;
+
+    text-align: center;
+
+    color: var(--ink-color);
+}
+</style>
